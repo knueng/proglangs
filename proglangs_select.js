ad3db97c67d8eb872cf8d2888c74a4db480dc1aa
knueng <knadiz@gmail.com>
2013-09-19 20:31:40 +0200

    Update proglangs_select.js
    La til JavaScript kode til oppgave 2, LAB2

--- proglangs_select.js
+++ proglangs_select.js
@@ -5,7 +5,12 @@ alert(element_unordered_list);
 function printToOutputBox(selectobj) {
 var e_proglangs = document.getElementById("proglangs");
 var e_outputbox = document.getElementById("outputbox");
-e_outputbox.innerHTML = "Du har valgt programmeringsspråket " + e_proglangs.
+e_outputbox.innerHTML = "Du har valgt programmeringsspråket " + e_proglangs.		// Endrer innholdet i outputboxen
 options[selectobj.selectedIndex].text;
 
+}
+
+function myFunction()
+{
+alert("Beklager, denne siden er under konstruksjon!");				// Gjør at myFunction sender et varsel.
 }
