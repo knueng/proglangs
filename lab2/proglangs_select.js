f2f9b44432c67db783a76a41770fb1ddbbd4c3e8
knueng <knadiz@gmail.com>
2013-10-12 01:14:40 +0200

    Create proglangs_select.js
    JavaScript kode for lab2

---
+++ lab2/proglangs_select.js
@@ -0,0 +1,14 @@
+
+
+function printToOutputBox(selectobj) {
+var e_proglangs = document.getElementById("proglangs");
+var e_outputbox = document.getElementById("outputbox");
+e_outputbox.innerHTML = "Du har valgt programmeringsspråket " + e_proglangs.		// Endrer innholdet i outputboxen
+options[selectobj.selectedIndex].text;
+
+}
+
+function myFunction()
+{
+alert("Beklager, denne siden er under konstruksjon!");				// Gjør at myFunction sender et varsel.
+}
